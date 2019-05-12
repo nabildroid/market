aedb6558c18d9de851ae6d8d15e2189093587977
nabil <pni20156789@gmail.com>
2019-05-12 20:51:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <nav class="contianer">
+      <div id="logo">
+        <h2>
+          <span>M</span>arket
+        </h2>
+      </div>
+      <div id="search">
+        <input type="text" placeholder="search">
+      </div>
+      <div id="tool">
+        <ul>
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">creat account</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Market",
+  components: {}
+};
+</script>
+
+<style>
+</style>
